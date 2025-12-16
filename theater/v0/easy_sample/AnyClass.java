8014ffd7fc246700aa2af126625f10d4ea9ad4d6
net1506 <net1506@naver.com>
2025-12-16 10:51:30 +0900

    [feat] Easy Sample 추가

---
+++ theater/v0/easy_sample/AnyClass.java
@@ -0,0 +1,14 @@
+package v0.easy_sample;
+
+public class AnyClass {
+    void anyMethodV1(Rectangle rectangle, int multiple) {
+        // 중복 코드 발생할 가능성 ↑↑↑
+        rectangle.setBottom(rectangle.getBottom() * multiple);
+        rectangle.setRight(rectangle.getRight() * multiple);
+    }
+
+    void anyMethodV2(Rectangle rectangle, int multiple) {
+        // 중복 코드 발생할 가능성 ↓↓↓
+        rectangle.enlarge(multiple);
+    }
+}
