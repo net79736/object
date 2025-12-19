6bd33def219a83e185724470e83e519dd7ae7916
net1506 <net1506@naver.com>
2025-12-19 13:24:41 +0900

    문제6번 풀이: 1-2

---
+++ theater/test/claude6_2/Order.java
@@ -0,0 +1,24 @@
+package test.claude6_2;
+
+import java.util.List;
+
+public class Order {
+    private double amount;
+
+    public double getAmount() {
+        return amount;
+    }
+
+    /**
+     * 주문 리스트의 총 금액을 계산합니다.
+     * 
+     * @param orders 주문 리스트
+     * @return 주문 리스트의 총 금액
+     */
+    public static double calculateTotalAmount(List<Order> orders) {
+        if (orders == null || orders.isEmpty()) {
+            return 0;
+        }
+        return orders.stream().mapToDouble(Order::getAmount).sum();
+    }
+}
